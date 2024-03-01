7002d11e7f7b4fb3480817b4de238483d9c65e08
Peishino <gabriel.moreira419@gmail.com>
2024-02-29 22:01:16 -0300

    Criação de botões para seleção de composição.

--- AnaliseGeral.R
+++ AnaliseGeral.R
@@ -1,13 +1,104 @@
 library(jsonlite)
-
+setwd("C:/Users/Gabriel/Documents/GitHub/Analise_TFT")
 dados <- fromJSON("TFT_DataS10.json")
 
 
-teste <- dados$units
+teste <- dados$units$TFT10_Gnar$ability
 
 nomes_das_listas <- sapply(teste, function(lista) lista[1])
 print(nomes_das_listas)
 
-names(dados$units)
+names(dados$units$tft10)
+nomes_das_unidades <- names(dados$units)
+nomes_das_unidades[1]
+#webscraping para pegar as traits de cada personagem:
 
-dados$units$TFT10_Gnar
+library(rvest)
+library(stringr)
+
+url <- "https://tactics.tools/info/units"
+html <- read_html(url)
+
+textosWS <- html %>%
+  html_elements(xpath = '//*[@id="content-container"]/div/div/div/a/div') %>%
+  html_text2()
+
+Vetor <- str_split(textosWS,"\n")
+
+max_length <- max(sapply(Vetor,length))
+
+Vetor <- lapply(Vetor, function(x){
+  length(x)<- max_length
+  x
+})
+
+df <- do.call(rbind, Vetor)
+colnames(df) <- c("Personagem","Custo","Trait1","Trait2","Trait3")
+
+df<-as.data.frame(df)
+
+
+# Mapear Traits e Tier
+Traits <- lapply(df$Personagem, function(personagem) {
+  traits <- c(df[df$Personagem == personagem, c("Trait1", "Trait2", "Trait3")])
+  traits <- traits[!is.na(traits)]
+  return(traits)
+})
+
+Tier <- df$Custo
+
+for (i in seq_along(dados$units)) {
+  personagem <- dados$units[[i]]$name
+  if (personagem %in% df$Personagem) {
+    dados$units[[i]]$Traits <- Traits[[match(personagem, df$Personagem)]]
+    dados$units[[i]]$Tier <- Tier[match(personagem, df$Personagem)]
+  }
+}
+
+write_json(dados, "TFT_DataS10.json")
+
+# Webscaping das imagens :D
+
+imageslinks <- html %>%
+  html_elements(".border-primary-6 .object-cover")%>%
+  html_attr("src")
+
+
+image_names <- sapply(imageslinks, function(url) {
+  # Divide a URL pelo caractere "/"
+  parts <- strsplit(url, "/")[[1]]
+  # Pega a última parte da URL (que contém o nome do arquivo) e divide pelo caractere "."
+  filename_parts <- strsplit(tail(parts, 1), "\\.")[[1]]
+  # Retorna o nome do arquivo sem a extensão
+  paste0(filename_parts[1])
+})
+
+library(httr)
+for (i in seq_along(imageslinks)) {
+  GET(imageslinks[i], write_disk(paste0(image_names[i], ".jpg"),overwrite = T))
+ }
+
+image_names
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app.R
+++ app.R
@@ -1,52 +1,114 @@
 library(shiny)
+library(shinythemes)
 library(jsonlite)
+library(tidyverse)
 
+getwd()
 dados <- fromJSON("TFT_DataS10.json")
 
-nomes_das_unidades <- names(dados$units)
-nomes_dos_labels <- sapply(teste, function(lista) lista[1])
+
+# Ordenação pela coluna Tier
+ordenacao_tier <- order(sapply(dados$units, function(lista) lista$Tier))
+
+nomes_das_unidades <- names(dados$units)[ordenacao_tier]
+nomes_dos_labels <- sapply(dados$units, function(lista) lista$name)[ordenacao_tier]
+
+# Função para obter a cor da borda com base no Tier
+getBorderColor <- function(tier) {
+  case_when(
+    tier == 2 ~ "green",
+    tier == 3 ~ "blue",
+    tier == 4 ~ "purple",
+    tier == 5 ~ "yellow",
+    TRUE ~ "black"  # Cor padrão para outros Tiers
+  )
+}
 
 ui <- fluidPage(
+  theme = shinytheme("darkly"),
   titlePanel("Seleção de Personagem TFT"),
   
   sidebarLayout(
     sidebarPanel(
       fluidRow(
+        selectInput("num_estrelas", "Número de Estrelas", c("1 Estrela", "2 Estrelas", "3 Estrelas")),
         lapply(seq_along(nomes_das_unidades), function(i) {
-          actionButton(inputId = nomes_das_unidades[i], label = nomes_dos_labels[i])
-        }),
-        actionButton(inputId = "calcular_hp", label = "Calcular HP Total")
+          actionButton(
+            inputId = paste0("btn_", nomes_das_unidades[i]),
+            label = nomes_dos_labels[i],
+            style = sprintf("border-color: %s; background-image: url('%s.jpg'); background-size: cover; color: white; width: 110px; height:60px;", 
+                            getBorderColor(dados$units[[nomes_das_unidades[i]]]$Tier),
+                            nomes_das_unidades[i])
+          )
+        })
       )
     ),
     
     mainPanel(
-      textOutput("info_personagem")
+      textOutput("info_personagem"),
+      tableOutput("table_traits")
+    )
+  ),
+  
+  # Adicionar estilo CSS para as bordas dos botões
+  tags$head(
+    tags$style(
+      HTML(
+        paste0(
+          lapply(seq_along(nomes_das_unidades), function(i) {
+            sprintf("#btn_%s { border-width: 2px; border-radius: 5px; }", nomes_das_unidades[i])
+          }),
+          collapse = "\n"
+        )
+      )
     )
   )
 )
 
 server <- function(input, output) {
-  selecao_personagens <- reactiveVal(character(0))  # Inicializa uma lista vazia para armazenar os personagens selecionados
+  selecao_personagens <- reactiveVal(list())  # Inicializa uma lista vazia para armazenar os personagens selecionados
   
   observe({
     lapply(nomes_das_unidades, function(personagem) {
-      eventExpr <- reactive(input[[personagem]])
+      eventExpr <- reactive(input[[paste0("btn_", personagem)]])
       observeEvent(eventExpr(), {
-        if (personagem %in% selecao_personagens()) {
-          selecao_personagens(selecao_personagens()[-which(selecao_personagens() == personagem)])
+        num_estrelas <- as.numeric(substr(input$num_estrelas, 1, 1))
+        temp_selecao <- selecao_personagens()
+        
+        if (personagem %in% names(temp_selecao)) {
+          temp_selecao[[personagem]] <- num_estrelas
         } else {
-          selecao_personagens(c(selecao_personagens(), personagem))
+          temp_selecao[[personagem]] <- num_estrelas
         }
+        
+        selecao_personagens(temp_selecao)
       }, ignoreInit = TRUE)
     })
   })
   
   output$info_personagem <- renderText({
     if (length(selecao_personagens()) > 0) {
-      hp_total <- sum(sapply(selecao_personagens(), function(personagem) dados$units[[personagem]]$stats$hp))
-      paste("Você selecionou os personagens:", paste(selecao_personagens(), collapse = ", "), "HP Total:", hp_total)
+      hp_total <- sum(sapply(names(selecao_personagens()), function(personagem) {
+        dados$units[[personagem]]$stats$hp * (1.8^(selecao_personagens()[[personagem]] - 1))
+      }))
+      
+      paste("Você selecionou os personagens com estrelas:",
+            paste(names(selecao_personagens()), ": ", unlist(selecao_personagens()), " estrelas", collapse = ", "),
+            "HP Total:", hp_total)
     } else {
       "Selecione pelo menos um personagem."
+    }
+  })
+  
+  output$table_traits <- renderTable({
+    if (length(selecao_personagens()) > 0) {
+      traits <- sapply(names(selecao_personagens()), function(personagem) {
+        dados$units[[personagem]]$Traits
+      })
+      contagem_traits <- table(unlist(traits))
+      data.frame(Trait = names(contagem_traits), Contagem = as.numeric(contagem_traits))
+    } else {
+      data.frame(Trait = character(), Contagem = numeric())
     }
   })
 }
